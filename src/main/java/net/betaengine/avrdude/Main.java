2b6fe1d16f92dccb0d311765d303b776cfc8659b
George Hawkins <github@beta-engine.net>
2014-04-27 21:05:53 +0200

    Added "--names" argument to Main.

--- src/main/java/net/betaengine/avrdude/Main.java
+++ src/main/java/net/betaengine/avrdude/Main.java
@@ -4,18 +4,23 @@ import java.io.OutputStreamWriter;
 
 public class Main {
 	public static void main(String[] args) {
+	    boolean names = args.length == 1 && args[0].equals("--names");
+	    
+	    if (args.length != 0 && !names) {
+	        System.err.println("Usage: java " + Main.class.getName() + " [--names]");
+	        System.exit(1);
+	    }
 		ConfParser parser = new ConfParser();
 		Maker maker = new Maker();
 		
 		parser.parse(maker);
 		
 		IndentPrintWriter writer = new IndentPrintWriter(new OutputStreamWriter(System.out));
-		BodyPrinter bodyPrinter = new BodyPrinter(false);
-		NamePrinter namePrinter = new NamePrinter();
 		
-		namePrinter.printAll(writer, maker);
-		writer.println();
-		bodyPrinter.printAll(writer, maker);
+		// Warning: using non-strict JSON with hex formatted integer literals.
+		AbstractPrinter printer = names ? new NamePrinter() : new BodyPrinter(false);
+
+		printer.printAll(writer, maker);
 		
 		writer.flush();
 	}
