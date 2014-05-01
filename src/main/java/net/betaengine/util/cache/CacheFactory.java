73191f542247cbb6b2a8d37a8e4ba80ff3850bf0
George Hawkins <github@beta-engine.net>
2014-05-01 17:40:01 +0200

    Added log statement to CacheFactory.

--- src/main/java/net/betaengine/util/cache/CacheFactory.java
+++ src/main/java/net/betaengine/util/cache/CacheFactory.java
@@ -1,12 +1,19 @@
 package net.betaengine.util.cache;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class CacheFactory {
+    private final static Logger log = LoggerFactory.getLogger(CacheFactory.class);
+
     public static Cache getCache(String uuid) {
         try {
             String name = System.getProperty("betaengine.cache", NoCache.class.getName());
             Cache cache = (Cache)Class.forName(name).newInstance();
             
             cache.setUuid(uuid);
+            
+            log.info("created cache {}", name);
             
             return cache;
         } catch (Exception e) {
